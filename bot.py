de6bbc392109403c54df75f7c85d63137938d5db
catsneverdie <o.nefedev@ukr.net>
2018-11-02 12:49:19 +0200

    Добавление команды /postverse

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # настройки
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import apiai, json
+import apiai, json, awsmpg
 updater = Updater(token='700733396:AAEUM6LbSwG8YFXMKjfvXYK--BZjOFitwR8')
 dispatcher = updater.dispatcher
 
@@ -23,19 +23,27 @@ def textMessage(bot, update):
 	# если есть ответ от ИИ - посылаем юзеру
 	if response:
 		bot.send_message(chat_id=update.message.chat_id, text=response)
-
-
+# /sayhello
 def sayHello(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text='Привет! Кликни на это сообщение и нажми "Ответить", чтобы поговорить со мной.')
+
+# /postverse
+# отправляет сообщением случайно сгенерированное четверостишие
+def postPoem(bot, update):
+	poem = awsmpg.makePoem()
+	bot.send_message(chat_id=update.message.chat_id, text=poem)
+
 
 # хендлеры
 start_command_handler = CommandHandler('start', startCommand)
 text_message_handler = MessageHandler(Filters.text, textMessage)
 sayhello_handler = CommandHandler('sayhello', sayHello)
+postverse_handler = CommandHandler('postverse', postPoem)
 
 dispatcher.add_handler(start_command_handler)
 dispatcher.add_handler(text_message_handler)
 dispatcher.add_handler(sayhello_handler)
+dispatcher.add_handler(postverse_handler)
 
 # поиск обновлений
 updater.start_polling(clean='True')
